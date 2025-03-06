9844931d7fa1a97b3bc8f6a6406ab8ffc2c1775d
Godwin Uche <godwin.uche@paynovate.com>
2025-03-06 14:32:46 +0100

    Reviewed the test

--- tests/Feature/TaskTest.php
+++ tests/Feature/TaskTest.php
@@ -7,7 +7,14 @@ beforeEach(function () {
 });
 
 test('view create task screen', function () {
-    $this->get('/task')
+    $this->get('/task/create')
         ->assertStatus(200)
         ->assertSee('Create New Task');
+});
+
+test('view edit task screen', function () {
+    $task = Task::first();
+    $this->get(route('edit.task', ['task' => $task]))
+        ->assertStatus(200)
+        ->assertSee('Edit Task');
 });
